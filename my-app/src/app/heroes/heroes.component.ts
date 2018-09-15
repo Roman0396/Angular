f38d14eb49901a8a88f9796ae847437aa0f9e4f9
Roman Cherkasko <roman_cherkasko@epam.com>
2018-09-15 21:00:43 +0300

    divided the view logic to a new component

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import{Component, OnInit}from '@angular/core';
 import {HEROES}from '../mock-heroes';
-
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
