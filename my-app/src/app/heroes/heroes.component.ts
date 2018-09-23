72b8fabebb5b56b7a83d3fda75509eccf7d7a0fc
Roman Cherkasko <roman_cherkasko@epam.com>
2018-09-23 19:42:53 +0300

    refactored hero detail work

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -9,8 +9,6 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-
 
   constructor(private heroService: HeroService) {
   }
@@ -22,10 +20,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
